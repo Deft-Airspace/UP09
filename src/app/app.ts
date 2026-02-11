4d7d7cf69ef5b845585e61d91095a5ad9e076311
Deft-Airspace <k.bocharov2007@gmail.com>
2026-02-11 12:30:09 +0300

    Angular Day 3 Section 4

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './header/header';
 import { Section1 } from './section1/section1';
 import { Section2 } from './section2/section2';
 import { Section3 } from './section3/section3';
+import { Section4 } from "./section4/section4";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Section1, Section2, Section3],
+  imports: [Header, Section1, Section2, Section3, Section4],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
