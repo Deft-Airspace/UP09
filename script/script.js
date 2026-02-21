04c37a0e86434a080e298289df2113a73cbb4302
Deft_Airspace <k.bocharov2007@gmail.com>
2026-02-21 20:58:06 +0300

    Edit js for menu

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
 function menu() {
+	const but = document.getElementById('but')
 	const menu = document.getElementById('menu')
+	let active = true
 	if (window.innerWidth <= 768) {
 		menu.classList.toggle('menu_active')
+		but.classList.toggle('header_right_active')
+		but.classList.toggle('header_right')
 	}
 }
