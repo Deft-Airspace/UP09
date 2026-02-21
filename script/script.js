391b6757d34530759762816f0ae6b76c50b6665e
Deft_Airspace <k.bocharov2007@gmail.com>
2026-02-21 20:30:50 +0300

    Edit js and @media for 768px

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 function menu() {
-	const but = document.getElementById('but')
 	const menu = document.getElementById('menu')
 	if (window.innerWidth <= 768) {
 		menu.classList.toggle('menu_active')
